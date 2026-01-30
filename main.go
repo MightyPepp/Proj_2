cb9c5511fe33f7e8c8f26ac2c08434c7e5576658
Aleksandr_Samsonov <samsonovaleksandr1352006@gmail.com>
2026-01-30 12:47:27 +0300

    Пытаемся протестировать переписанный с гитхаба сервер через curl

--- main.go
+++ main.go
@@ -1,17 +1,173 @@
 package main
 
 import (
-	"net/http"
+	"Proj_2/taskstore"
+	"encoding/json"
 	"fmt"
 	"log"
 	"mime"
-	"os"
+	"net/http"
 	"strconv"
 	"time"
-	"Proj_2/internal\taskstore"
 )
+
+type taskServer struct {
+	store *taskstore.TaskStore
+}
+
+func NewTaskServer() *taskServer {
+	store := taskstore.New()
+	return &taskServer{store: store}
+}
+
+func (ts *taskServer) createTaskHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("handling task create at %s\n", r.URL.Path)
+
+	type RequestTask struct {
+		Text string `json:"text"`
+		Tags []string `json:"tags"`
+		Due time.Time `json:"due"`
+	}
+
+	type ResponseId struct {
+		Id int `json:"id"`
+	}
+
+	contentType := r.Header.Get("Content-Type")
+	mediatype, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if mediatype != "application/json" {
+		http.Error(w, "except application/json Content-Type", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	var rt RequestTask
+	if err := dec.Decode(&rt); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id := ts.store.CreateTask(rt.Text, rt.Tags, rt.Due)
+	js, err := json.Marshal(ResponseId{Id: id})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (ts *taskServer) getAllTasksHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("handling get all tasks at %s\n", r.URL.Path)
+
+	allTasks := ts.store.GetAllTasks()
+	js, err := json.Marshal(allTasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (ts *taskServer) getTaskHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("handling get task at %s\n", r.URL.Path)
+
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+	}
+
+	task, err := ts.store.GetTask(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	js, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (ts *taskServer) deleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("handling delete task at %s\n", r.URL.Path)
+
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
+	}
+
+	err = ts.store.DeleteTask(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+	}
+}
+
+func (ts *taskServer) deleteAllTasksHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("handling delete all tasks at %s\n", r.URL.Path)
+	ts.store.DeleteAllTasks()
+}
+
+func (ts *taskServer) tagHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("handling tasks by tag at %s\n", r.URL.Path)
+
+	tag := r.PathValue(("tag"))
+
+	tasks := ts.store.GetTaskByTag(tag)
+	js, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (ts *taskServer) dueHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("handling tasks by due at %s\n", r.URL.Path)
+
+	badRequestError := func() {
+		http.Error(w, fmt.Sprintf("except /due/<year>/<month>/<day>, got %v", r.URL.Path), http.StatusBadRequest)
+	}
+
+	year, errYear := strconv.Atoi(r.PathValue("year"))
+	month, errMonth := strconv.Atoi(r.PathValue("month"))
+	day, errDay := strconv.Atoi(r.PathValue("day"))
+	if errYear != nil || errMonth != nil || errDay != nil || month < int(time.January) || month > int(time.December) {
+		badRequestError()
+		return
+	}
+
+	tasks := ts.store.GetTaskByDueData(year, time.Month(month), day)
+	js, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
 
 func main() {
 	mux := http.NewServeMux()
 	server := NewTaskServer()
+
+	mux.HandleFunc("POST /task/", server.createTaskHandler)
+	mux.HandleFunc("GET /task/", server.getAllTasksHandler)
+	mux.HandleFunc("DELETE /task/", server.deleteAllTasksHandler)
+	mux.HandleFunc("GET /task/{id}/", server.getTaskHandler)
+	mux.HandleFunc("DELETE /task/{id}/", server.deleteTaskHandler)
+	mux.HandleFunc("GET /tag/{tag}/", server.tagHandler)
+	mux.HandleFunc("GET /due/{year}/{month}/{day}/", server.dueHandler) 
+	
+	http.ListenAndServe(":8080", nil)
 }
